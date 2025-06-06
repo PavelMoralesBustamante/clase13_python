23a85c2620645e7acaf8fc00295d253058c4cee9
PavelMoralesBustamante <136739857+PavelMoralesBustamante@users.noreply.github.com>
2025-06-06 11:04:06 -0400

    Version3
    Contador de números pares

--- Creación_números_aleatorios.py
+++ Creación_números_aleatorios.py
@@ -2,8 +2,15 @@ import random #libreria para llamar y crear numeros random
 
 numeros = [] #Colección vacía
 
-for i in range(100): #Ciclo para repetir 100 veces las iteraciones
+for i in range(10): #Ciclo para repetir 100 veces las iteraciones
     n = random.randint(1,100) #Crea el numero entre 1 y 100
     numeros.append(n) #Guardamos en la lista el numero aleatorio
 
 print(numeros) #Visualizamos lista
+
+#Contador de números pares
+cont = 0
+for i in range(len(numeros)):
+    if numeros[i]%2==0:
+        cont+=1
+print(f"Cantidad de números pares : {cont}")
